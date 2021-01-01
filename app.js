972533873305d2f9d3ca84588e93d89098df594e
jainanuj261 <jainanuj261@gmail.com>
2021-01-01 14:22:27 +0530

    modified ap.js

--- app.js
+++ app.js
@@ -1,38 +1,44 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 //my Routes
-const authRoute = require('./routes/auth');
-const userRoute = require('./routes/user');
-const categoryRoutes = require('./routes/category');
-const productRoutes = require('./routes/product');
-const orderRoutes = require('./routes/order');
-const stripeRoutes = require('./routes/stripepayment');
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+const orderRoutes = require("./routes/order");
+const stripeRoutes = require("./routes/stripepayment");
 
 //DB Connection
-mongoose.connect(process.env.DATABASE,{useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true}).then(()=>{console.log("DB CONNECTED")}).catch(err => console.log( err ));
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("DB CONNECTED");
+  })
+  .catch((err) => console.log(err));
 
 //Middlewares
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
 
+const port = process.env.port || 8000;
 
-const port = process.env.PORT ||8000;
+app.use("/api", authRoute);
+app.use("/api", userRoute);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+app.use("/api", orderRoutes);
+app.use("/api", stripeRoutes);
 
-app.use('/api',authRoute);
-app.use('/api',userRoute);
-app.use('/api',categoryRoutes);
-app.use('/api',productRoutes);
-app.use('/api',orderRoutes);
-app.use('/api',stripeRoutes);
-
-
-app.listen(port,()=>console.log(`App is running at ${port}`));
+app.listen(port, () => console.log(`App is running at ${port}`));
